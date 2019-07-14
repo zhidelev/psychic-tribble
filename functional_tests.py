abc3eddf079be21c7ac17980ce7bfee259a9ea28
andreiz <mcjanz@gmail.com>
2019-07-14 14:00:32 +0300

    jrnevjnerj

--- functional_tests.py
+++ functional_tests.py
@@ -26,10 +26,15 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-        "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test')
 
--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,11 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/'
@@ -20,6 +25,4 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
         
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
         
