3da83eab9b2ac52f196ac74e61ca7995d830555a
andreiz <mcjanz@gmail.com>
2019-07-15 20:11:16 +0300

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,21 @@ from django.template.loader import render_to_string
 from lists.models import Item
 
 # Create your tests here.
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retriving_items(self):
@@ -29,15 +44,6 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
@@ -55,15 +61,4 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/'
-)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
-        
-        
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
