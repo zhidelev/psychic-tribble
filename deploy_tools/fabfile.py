4553c7c16d03bdefa76964a8a34b4a597c1db6a2
andreiz <mcjanz@gmail.com>
2019-07-18 22:01:56 +0300

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -8,21 +8,21 @@ def _get_latest_source():
     if exists('.git'):
         run('git fetch')
     else:
-        run(f"git clone {REPO_URL}")
+        run(f"git clone {REPO_URL} .")
     current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f"git reset --hard {current_commit}")
 
 def _update_virtualenv():
     if not exists("virtualenv/bin/pip"):
         run(f"python3.6 -m venv virtualenv")
-    run("virtualenv/bin/pip install -r requirements.txt")
+    run("./virtualenv/bin/pip install -r requirements.txt")
 
 def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f"SITENAME={env.host}")
     current_contents = run("cat .env")
     if "DJANGO_SECRET_KEY" not in current_contents:
-        new_secret = "".join(random.SystemRandom().choice("vhbjkvbkjreb328y5839271598"), k=50))
+        new_secret = "".join(random.SystemRandom().choices("vhbjkvbkjreb328y5839271598", k=50))
         append(".env", f"DJANGO_SECRET_KEY={new_secret}")
 
 def _update_static_files():
